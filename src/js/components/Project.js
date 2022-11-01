43638da38ba956fbd421a925e7e3e8f206111e87
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-11-01 17:54:56 +0700

    rewrite 'Project' component as a class

--- src/js/components/Project.js
+++ src/js/components/Project.js
@@ -1,58 +1,65 @@
 import { component } from 'reefjs';
 import * as model from '../model';
 
-const _parentElement = document.querySelector('.project-window');
+class Project {
+  _parentElement = document.querySelector('.project-window');
 
-function _template() {
-  if (!Object.keys(model.state.activeProject).length) return '';
+  constructor() {
+    this._addHandlerMakeProjectActive();
 
-  return `
-    <div class="project" data-id="${model.state.activeProject.id}">
-      <p class="project__title">Title: ${model.state.activeProject.title}</p>
-      <p class="project__description">Description: ${
-        model.state.activeProject.description
-      }</p>
-      <p class="project__due-date">Due date: ${
-        model.state.activeProject.dueDate
-          ? model.state.activeProject.dueDate
-          : 'No due date'
-      }</p>
-      <p>TASKS:</p>
-      <ul class="tasks">
-        ${_generateTasksMarkup(model.state.activeProject.tasks)}
-      </ul>
-      <button class="btn--project-controls">...</button>
-      <button class="btn--add-task">Add task</button>
-    </div>
-  `;
+    component(this._parentElement, this._template.bind(this));
+  }
+
+  _template() {
+    if (!Object.keys(model.state.activeProject).length) return '';
+
+    return `
+      <div class="project" data-id="${model.state.activeProject.id}">
+        <p class="project__title">Title: ${model.state.activeProject.title}</p>
+        <p class="project__description">Description: ${
+          model.state.activeProject.description
+        }</p>
+        <p class="project__due-date">Due date: ${
+          model.state.activeProject.dueDate
+            ? model.state.activeProject.dueDate
+            : 'No due date'
+        }</p>
+        <p>TASKS:</p>
+        <ul class="tasks">
+          ${this._generateTasksMarkup(model.state.activeProject.tasks)}
+        </ul>
+        <button class="btn--project-controls">...</button>
+        <button class="btn--add-task">Add task</button>
+      </div>
+    `;
+  }
+
+  _generateTasksMarkup(tasks) {
+    return tasks
+      .map(function (task) {
+        return `
+          <li class="task" data-id="${task.id}">
+            <p class="task__title">Title: ${task.title}</p>
+            <p class="task__description">Description: ${task.description}</p>
+            <p class="task__due-date">Due date: ${
+              task.dueDate ? task.dueDate : 'No due date'
+            }</p>
+          <button class="btn--task-controls">...</button>
+          </li>
+        `;
+      })
+      .join('');
+  }
+
+  _addHandlerMakeProjectActive() {
+    ['hashchange', 'load'].forEach(e =>
+      window.addEventListener(e, function () {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+        model.setProjectAsActive(id);
+      })
+    );
+  }
 }
 
-function _generateTasksMarkup(tasks) {
-  return tasks
-    .map(function (task) {
-      return `
-        <li class="task" data-id="${task.id}">
-          <p class="task__title">Title: ${task.title}</p>
-          <p class="task__description">Description: ${task.description}</p>
-          <p class="task__due-date">Due date: ${
-            task.dueDate ? task.dueDate : 'No due date'
-          }</p>
-        <button class="btn--task-controls">...</button>
-        </li>
-      `;
-    })
-    .join('');
-}
-
-component(_parentElement, _template);
-
-// EVENT LISTENERS
-
-// Set the project as active (i.e., render it)
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, function () {
-    const id = window.location.hash.slice(1);
-    if (!id) return;
-    model.setProjectAsActive(id);
-  })
-);
+export default new Project();
