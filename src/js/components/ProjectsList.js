02fbdba914cb9aa2fbbdef2f29cbc2bad9683b23
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-11-01 17:47:39 +0700

    rewrite 'ProjectList' component as a class

--- src/js/components/ProjectsList.js
+++ src/js/components/ProjectsList.js
@@ -1,26 +1,32 @@
 import { component } from 'reefjs';
 import * as model from '../model';
 
-const _parentElement = document.querySelector('.projects-list');
+class ProjectList {
+  _parentElement = document.querySelector('.projects-list');
 
-function _template() {
-  return model.state.projects
-    .map(function (project) {
-      return `
-        <li data-id="${project.id}" class="project-item">
-          <a href="#${project.id}" class="project-item--link
-          ${
-            project.id === model.state.activeProject.id
-              ? 'project-item--active'
-              : ''
-          }">
-            <span>${project.title}</span>
-          </a>
-          <button class="btn--project-controls">...</button>
-        </li>
-      `;
-    })
-    .join('');
+  constructor() {
+    component(this._parentElement, this._template);
+  }
+
+  _template() {
+    return model.state.projects
+      .map(function (project) {
+        return `
+          <li data-id="${project.id}" class="project-item">
+            <a href="#${project.id}" class="project-item--link
+            ${
+              project.id === model.state.activeProject.id
+                ? 'project-item--active'
+                : ''
+            }">
+              <span>${project.title}</span>
+            </a>
+            <button class="btn--project-controls">...</button>
+          </li>
+        `;
+      })
+      .join('');
+  }
 }
 
-component(_parentElement, _template);
+new ProjectList();
