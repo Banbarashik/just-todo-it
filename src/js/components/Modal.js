5545d5bf17b9c6c6b893b46d1a5630a187b2220c
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-11-05 11:38:51 +0700

    make some changes to the template

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -5,30 +5,36 @@ export default class Modal {
     if (!this.state.isModalOpened) return '';
 
     return `
-      <h3>${cap1stLtr(this._modalType)} ${this._itemType}</h3>
-      <form class="${this._modalType}-${this._itemType}-form">
-        <div class="form-field">
-          <label>Title</label>
-          <input name="title" />
-        </div>
-        <div class="form-field">
-          <label>Description</label>
-          <textarea name="description" cols="30" rows="5"></textarea>
-        </div>
-        <div class="form-field">
-          <label>Due date</label>
-          <input type="datetime-local" name="dueDate" />
-        </div>
-        ${
-          this._itemType === 'task'
-            ? `<select name="project">${this._generateProjectsList()}</select>`
-            : ''
-        }
-        <button type="button" class="btn--close-modal">Cancel</button>
-        <button type="submit">${
-          this._modalType === 'edit' ? 'Save' : cap1stLtr(this._modalType)
-        }</button>
-      </form>
+      <div class="modal">
+        <h3>${cap1stLtr(this._modalType)} ${this._itemType}</h3>
+        <form class="${this._modalType}-${this._itemType}-form">
+          <div class="form-fields-block">
+            <div class="form-field--title">
+              <label>Title</label>
+              <input name="title" />
+            </div>
+            <div class="form-field--desc">
+              <label>Description</label>
+              <textarea name="description"></textarea>
+            </div>
+            <div class="form-field--due-date">
+              <label>Due date</label>
+              <input type="datetime-local" name="dueDate" />
+            </div>
+          </div>
+          ${
+            this._itemType === 'task'
+              ? `<select name="project">${this._generateProjectsList()}</select>`
+              : ''
+          }
+          <div class="form-btns-block">
+            <button type="button" class="btn--close-modal">Cancel</button>
+            <button type="submit">${
+              this._modalType === 'edit' ? 'Save' : cap1stLtr(this._modalType)
+            }</button>
+          </div>
+        </form>
+      </div>
     `;
   }
 
