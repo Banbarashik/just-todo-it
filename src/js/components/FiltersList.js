2951ed6b1d1df2356382166d9a6e37297ae9dc57
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-11-09 15:45:31 +0700

    fix a typo in 'FiltersList' component's template

--- src/js/components/FiltersList.js
+++ src/js/components/FiltersList.js
@@ -15,9 +15,9 @@ class FiltersList {
       }">
         <a href="#inbox" class="project-item--link"><span>Inbox</span></a>
       </li>
-      <li class="filter-today project-item" ${
+      <li class="filter-today project-item ${
         model.state.activeProject.id === 'today' ? 'active' : ''
-      }>
+      }">
         <a href="#today" class="project-item--link"><span>Today</span></a>
       </li>
     `;
