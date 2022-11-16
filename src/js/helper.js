abbc2d9a52fe7107ad577f9ca9d01c8c3d527a77
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-11-16 14:27:04 +0700

    create 'isToday' and 'formatDate' helper functions

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,1 +1,43 @@
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
 export const cap1stLtr = str => str.replace(/^\w/, c => c.toUpperCase());
+
+export const isToday = date =>
+  date.toDateString() === new Date().toDateString();
+
+export const formatDate = date => {
+  const now = new Date();
+
+  const monthIndex = date.getMonth();
+
+  const dayOfMonth = date.getDate();
+  const month = months[monthIndex];
+  const year = date.getFullYear();
+  const time =
+    `${date.getHours()}`.padStart(2, 0) +
+    ':' +
+    `${date.getMinutes()}`.padStart(2, 0);
+
+  if (year === now.getFullYear() && monthIndex === now.getMonth()) {
+    if (dayOfMonth === now.getDate() - 1) return 'Yesterday';
+    if (dayOfMonth === now.getDate()) return 'Today';
+    if (dayOfMonth === now.getDate() + 1) return 'Tomorrow';
+  }
+
+  return `${dayOfMonth} ${month} ${year !== now.getFullYear() ? year : ''} ${
+    time ? time : ''
+  }`;
+};
