13e647115cd5b34287f2dd36918c8de141179ca1
Banbarashik <odinokyskitalec@yahoo.com>
2023-06-25 22:07:45 +0400

    fix: curChar counts whitespace in the beggining as a char

--- src/js/components/EditModal.js
+++ src/js/components/EditModal.js
@@ -19,8 +19,9 @@ export default class EditModal extends Modal {
     //prettier-ignore
     const item = model[cap1stLtr(this._itemType) + 'Controls'].state[this._itemType];
 
-    Modal.state[this._itemType].title.curChar = item.title.length;
-    Modal.state[this._itemType].description.curChar = item.description.length;
+    Modal.state[this._itemType].title.curChar = item.title.trimStart().length;
+    // prettier-ignore
+    Modal.state[this._itemType].description.curChar = item.description.trimStart().length;
 
     const form = this._parentElement.querySelector('form');
     const inputTitle = form.querySelector('[name="title"]');
